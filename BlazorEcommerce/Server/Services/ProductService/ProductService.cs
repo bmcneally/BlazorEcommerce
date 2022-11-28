2d5d0bef9063df66c08bf9bf23d8c1c6daedda6e
Ben McNeally <bmcneally@points-north.com>
2022-11-27 20:50:24 -0600

    Added variant pricing to list

--- BlazorEcommerce/Server/Services/ProductService/ProductService.cs
+++ BlazorEcommerce/Server/Services/ProductService/ProductService.cs
@@ -15,7 +15,7 @@ namespace BlazorEcommerce.Server.Services.ProductService
         {
             var response = new ServiceResponse<List<Product>>
             {
-                Data = await _context.Products.ToListAsync()
+                Data = await _context.Products.Include(p => p.Variants).ToListAsync()
             };
 
             return response;
@@ -25,7 +25,11 @@ namespace BlazorEcommerce.Server.Services.ProductService
         {
             var response = new ServiceResponse<Product>();
 
-            var product = await _context.Products.FindAsync(productId);
+            var product = await _context.Products
+                .Include(p => p.Variants)
+                    .ThenInclude(v => v.ProductType)
+                .FirstOrDefaultAsync(p => p.Id == productId);
+
             if (product == null)
             {
                 response.Success = false;
@@ -45,6 +49,7 @@ namespace BlazorEcommerce.Server.Services.ProductService
             {
                 Data = await _context.Products
                     .Where(x => x.Category.Url.ToLower() == categoryUrl.ToLower())
+                    .Include(p => p.Variants)
                     .ToListAsync()
             };
 
